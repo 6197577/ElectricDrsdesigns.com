0e1b289dbec3fe2302f2f148f886f268ccd6cdf3
Doc <admin@electricdrs.com>
2025-11-26 17:58:35 -0500

    Update LocationCard.tsx

--- src/components/LocationCard.tsx
+++ src/components/LocationCard.tsx
@@ -1,2 +1,20 @@
+import Image from 'next/image';
+import styles from '../styles/LocationCard.module.css';
 
-
+interface Props {
+  name: string;
+  imageUrl: string;
+  pageUrl: string;
+}
+
+const LocationCard = ({ name, imageUrl, pageUrl }: Props) => {
+  return (
+    <Link href={pageUrl} className={styles.card}>
+      <Image src={imageUrl} alt={name} layout="fill" objectFit="cover" />
+      <div className={styles.overlay}>
+        <h3>{name}</h3>
+      </div>
+    </Link>
+  );
+};
+export default LocationCard;
